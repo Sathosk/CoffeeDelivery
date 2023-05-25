17e99d495858ea95f678ec26079b8358574242a6
Tiago <sathoskath@gmail.com>
2023-05-25 20:57:46 -0300

    first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    a, button {
+        all: unset;
+        cursor: pointer;
+    }
+`;
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,90 @@
+export const defaultTheme = {
+    white: '#fff',
+
+    'base-title': '#272221',
+    'base-subtitle': '#403937',
+    'base-text': '#574f4d',
+    'base-label': '#8d8686',
+    'base-hover': '#d7d5d5',
+    'base-button': '#e6e5e5',
+    'base-input': '#ededed',
+    'background': 'fafafa',
+
+    'yellow-dark': '#C47f17',
+    'yellow': '#dbac2c',
+    'yellow-light': '#f1e9c9',
+    'purple-dark': '#4b2995',
+    'purple': '#8047f8',
+    'purple-light': '#ebe5f9',
+
+    fonts: {
+        baloo2: {
+            font: "font-family: 'Baloo 2', cursive;",
+            lineHeight: '130%',
+            
+            titleXL: {
+                fontSize: '48px',
+                fontWeight: '800'
+            },
+            titleL: {
+                fontSize: '32px',
+                fontWeight: '800'
+            },
+            titleM: {
+                fontSize: '24px',
+                fontWeight: '800'
+            },
+            titleS: {
+                fontSize: '20px',
+                fontWeight: '700'
+            },
+            titleXS: {
+                fontSize: '18px',
+                fontWeight: '700'
+            },
+        },
+
+        roboto: {
+            font: "font-family: 'Roboto', sans-serif;",
+
+            textL: {
+                fontSize: '20px',
+                lineHeight: '130%',
+                fontWeightRegular: '400',
+                fontWeightBold: '700'
+            },
+            textM: {
+                fontSize: '16px',
+                lineHeight: '130%',
+                fontWeightRegular: '400',
+                fontWeightBold: '700'
+            },
+            textS: {
+                fontSize: '14px',
+                lineHeight: '130%',
+                fontWeightRegular: '400',
+            },
+            textXS: {
+                fontSize: '12px',
+                lineHeight: '130%',
+                fontWeightBold: '700'
+            },
+
+            tag: {
+                fontSize: '10px',
+                lineHeight: '130%',
+                fontWeightBold: '700'
+            },
+            buttonL: {
+                fontSize: '14px',
+                lineHeight: '160%',
+                fontWeightBold: '700'
+            },
+            buttonM: {
+                fontSize: '12px',
+                lineHeight: '160%',
+                fontWeightBold: '400'
+            },
+        }
+    }
+}
